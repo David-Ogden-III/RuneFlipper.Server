c6fbea5dda46aa3eed85a428b38d4e851b9fae2a
David Ogden <david-ogden699@hotmail.com>
2024-10-11 16:33:32 -0500

    Scaffolded DB into project then migrated identity back to DB.

---
+++ RuneFlipper.Server/Models/Selltype.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace RuneFlipper.Server.Models;
+
+[Table("selltypes")]
+[Index("Name", Name = "selltypes_name_key", IsUnique = true)]
+public partial class Selltype
+{
+    [Key]
+    [Column("id")]
+    [StringLength(3)]
+    public string Id { get; set; } = null!;
+
+    [Column("name")]
+    [StringLength(20)]
+    public string Name { get; set; } = null!;
+
+    [InverseProperty("SellType")]
+    public virtual ICollection<Trade> Trades { get; set; } = new List<Trade>();
+}
---
+++ RuneFlipper.Server/Models/Trade.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RuneFlipper.Server.Models;
+
+[Table("trades")]
+public partial class Trade
+{
+    [Key]
+    [Column("id")]
+    public string Id { get; set; } = null!;
+
+    [Column("buyprice")]
+    public long BuyPrice { get; set; }
+
+    [Column("sellprice")]
+    public long SellPrice { get; set; }
+
+    [Column("quantity")]
+    public int Quantity { get; set; }
+
+    [Column("buydatetime", TypeName = "timestamp without time zone")]
+    public DateTime BuyDateTime { get; set; }
+
+    [Column("selldatetime", TypeName = "timestamp without time zone")]
+    public DateTime SellDateTime { get; set; }
+
+    [Column("iscomplete")]
+    public bool IsComplete { get; set; }
+
+    [Column("characterid")]
+    public string CharacterId { get; set; } = null!;
+
+    [Column("itemid")]
+    public string ItemId { get; set; } = null!;
+
+    [Column("buytypeid")]
+    [StringLength(3)]
+    public string BuyTypeId { get; set; } = null!;
+
+    [Column("selltypeid")]
+    [StringLength(3)]
+    public string SellTypeId { get; set; } = null!;
+
+    [ForeignKey("BuyTypeId")]
+    [InverseProperty("Trades")]
+    public virtual Buytype BuyType { get; set; } = null!;
+
+    [ForeignKey("CharacterId")]
+    [InverseProperty("Trades")]
+    public virtual Character Character { get; set; } = null!;
+
+    [ForeignKey("ItemId")]
+    [InverseProperty("Trades")]
+    public virtual Item Item { get; set; } = null!;
+
+    [ForeignKey("SellTypeId")]
+    [InverseProperty("Trades")]
+    public virtual Selltype SellType { get; set; } = null!;
+}
