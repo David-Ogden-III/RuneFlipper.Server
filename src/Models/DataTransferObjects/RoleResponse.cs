4c38b85c812422fa9fd9f8c71a83aa6b96e94f1d
David Ogden <david-ogden699@hotmail.com>
2024-10-18 00:03:42 -0500

    Update Class Name to be less action dependent

--- src/Models/DataTransferObjects/RoleResponse.cs
+++ src/Models/DataTransferObjects/RoleResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace Models.DataTransferObjects;
 
-public class FetchRoleResponse
+public class RoleResponse
 {
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
