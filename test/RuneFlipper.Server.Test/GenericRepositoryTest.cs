6caf2b6383adce40070ba1f166a626e9a8dcf56c
David Ogden <david-ogden699@hotmail.com>
2024-10-18 00:24:03 -0500

    Created test file for the Generic Repository

---
+++ test/RuneFlipper.Server.Test/GenericRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace RuneFlipper.Server.Test;
+
+public class GenericRepositoryTest
+{
+    //[Fact]
+    //public void Insert_SingleEntity_EntityStateAdded()
+    //{
+    //    Trade trade = new();
+    //    RuneFlipperContext stubContext = new();
+    //    GenericRepository<Trade> tradeRepository = new(stubContext);
+
+    //    tradeRepository.Insert(trade);
+    //    EntityState state = stubContext.Entry(trade).State;
+
+    //    Assert.True(state == EntityState.Added);
+    //}
+
+    // Implement a mock dbContext
+
+}
