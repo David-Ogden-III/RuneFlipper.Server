3147c78fb3b14a368bd87ce3f747797116971b12
David Ogden <david-ogden699@hotmail.com>
2024-11-01 17:44:56 -0500

    refactor

--- src/Models/DataTransferObjects/OSTradeSummary.cs
+++ src/Models/DataTransferObjects/OSTradeSummary.cs
@@ -4,7 +4,7 @@ using Models.TradeFactory;
 namespace Models.DataTransferObjects;
 public class OSTradeSummary(Trade trade) : TradeSummary(trade)
 {
-    protected override double TaxRate { get; } = Constants.OSTaxRate;
-    protected override int[] TaxExemptIds { get; } = Constants.OSTaxExemptIds;
-    protected override long MaxTaxPerItem { get; } = Constants.OSMaxTaxPerItem;
+    protected override double TaxRate => Constants.OSTaxRate;
+    protected override int[] TaxExemptIds => Constants.OSTaxExemptIds;
+    protected override long MaxTaxPerItem => Constants.OSMaxTaxPerItem;
 }
